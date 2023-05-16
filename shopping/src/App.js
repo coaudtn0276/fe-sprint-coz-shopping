670e1bfc206467fad6725ae3003308d86caf2182
coaudtn0276 <coaudtn0276@gmail.com>
2023-05-16 13:57:11 +0900

    상품리스트 배치

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -3,42 +3,59 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+
 // import { Routes, Route, Link } from "react-router-dom";
+import axios from "axios";
+
 import Header from "./components/header";
 import Footer from "./components/footer";
 
+import dummydata from "./data/dummydata";
+import { useState } from "react";
+
 function App() {
+  const [data, setDate] = useState(dummydata);
+
+  console.log(data[1].image_url);
+
   return (
     <div className="App">
       <Header />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
 
-      <Container>
+      <Container className="shoppinglist">
+        <div className="shoppinglist-title"> 상품리스트</div>
         <Row>
-          <Col sm>sm=true</Col>
+          <Col sm>
+            <div className="shoppinglist-img">
+              <img className="shoppingimg" src={data[1].image_url} alt="" />
+            </div>
+            <div className="shoppinglist-text">
+              <div className="shop-title">{data[1].title}</div>
+              <div className="shop-right">
+                <div className="shop-percent">
+                  {data[1].discountPercentage}%
+                </div>
+                <div className="shop-price">{data[1].price} 원</div>
+              </div>
+            </div>
+          </Col>
           <Col sm>sm=true</Col>
           <Col sm>sm=true</Col>
           <Col sm>sm=true</Col>
         </Row>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
       </Container>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
       <Footer />
     </div>
---
+++ shopping/src/data/dummydata.js
@@ -0,0 +1,56 @@
+const dummydata = [
+  {
+    id: 86,
+    type: "Brand",
+    title: null,
+    sub_title: null,
+    brand_name: "칼하트",
+    price: null,
+    discountPercentage: null,
+    image_url: null,
+    brand_image_url:
+      "https://images.unsplash.com/photo-1622408298915-24d322badf9e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80",
+    follower: 6287,
+  },
+  {
+    id: 23,
+    type: "Product",
+    title: "뉴발란스990",
+    sub_title: null,
+    brand_name: null,
+    price: "229000",
+    discountPercentage: 25,
+    image_url:
+      "https://images.unsplash.com/photo-1667453799963-5509cd48986a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80",
+    brand_image_url: null,
+    follower: null,
+  },
+  {
+    id: 79,
+    type: "Exhibition",
+    title: "멍냥이도 꿀잠이 필요해",
+    sub_title: "반려동물을 위한 수면용품",
+    brand_name: null,
+    price: null,
+    discountPercentage: null,
+    image_url:
+      "https://images.unsplash.com/photo-1541781774459-bb2af2f05b55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2060&q=80",
+    brand_image_url: null,
+    follower: null,
+  },
+  {
+    id: 63,
+    type: "Category",
+    title: "원피스",
+    sub_title: null,
+    brand_name: null,
+    price: null,
+    discountPercentage: null,
+    image_url:
+      "https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1773&q=80",
+    brand_image_url: null,
+    follower: null,
+  },
+];
+
+export default dummydata;
