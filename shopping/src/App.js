ecfba0b527c49a5ceefb67315496b7cab1030bc8
coaudtn0276 <coaudtn0276@gmail.com>
2023-05-17 14:58:10 +0900

    모달창

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -16,35 +16,40 @@ import Shoppinglist from "./components/shopplinglist";
 
 function App() {
   const [data, setDate] = useState(dummydata);
-
+  // console.log(data[1].image_url);
   return (
     <div className="App">
       <Header />
-
       <Container className="shoppinglist">
         <div className="shoppinglist-title"> 상품리스트</div>
         <Row>
           {data.map((el, idx) => (
-            <Shoppinglist Col={Col} data={data} idx={idx} />
+            <Shoppinglist Col={Col} data={data} idx={idx} key={idx} />
           ))}
         </Row>
       </Container>
 
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+      <Container className="shoppinglist">
+        <div className="shoppinglist-title"> 북마크 리스트</div>
+        <Row>
+          {data.map((el, idx) => (
+            <Shoppinglist Col={Col} data={data} idx={idx} key={idx} />
+          ))}
+        </Row>
+      </Container>
+
+      {/* <div className="test">
+        <img src={data[1].image_url} alt="" className="modal" />
+      </div> */}
 
       <Footer />
     </div>
   );
+}
+function Modal({ data, idx }) {
+  <div>
+    <img src={data[1].image_url} alt="" className="modal" />
+  </div>;
 }
 
 export default App;
--- shopping/src/components/shopplinglist.js
+++ shopping/src/components/shopplinglist.js
@@ -1,41 +1,90 @@
+import { useState } from "react";
 import bookstar from "../img/Bookmark.svg";
+import closebutton from "../img/Vector.svg";
 
 const Shoppinglist = function Shoppinglist({ Col, data, idx }) {
+  const [clickimg, setClickimg] = useState(false);
+
+  function imgclick() {
+    setClickimg(!clickimg);
+    if (clickimg === true) {
+      console.log(1);
+    }
+  }
+
   return (
-    <Col sm>
-      <div className="shopping-items">
-        <div className="shoppinglist-img">
-          <img
-            className="shoppingimg"
-            src={
-              data[idx].image_url === null
-                ? data[idx].brand_image_url
-                : data[idx].image_url
-            }
-            alt=""
-          />
-          <img src={bookstar} alt="" className="bookstar" />
-        </div>
-        <div className="shoppinglist-text">
-          <div className="shop-title">{data[idx].title}</div>
-          <div className="shop-right">
-            <div className="shop-percent">
-              {data[idx].discountPercentage === null
-                ? null
-                : data[idx].discountPercentage + `%`}
-            </div>
-            <div className="shop-price">
-              {data[idx].price === null
-                ? null
-                : Number(data[idx].price)
-                    .toString()
-                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",") + `원`}
+    <>
+      <Col sm>
+        <div className="shopping-items">
+          <div className="shoppinglist-img">
+            <img
+              className="shoppingimg"
+              src={
+                data[idx].image_url === null
+                  ? data[idx].brand_image_url
+                  : data[idx].image_url
+              }
+              alt=""
+              onClick={() => {
+                imgclick();
+              }}
+            />
+            <img src={bookstar} alt="" className="bookstar" />
+          </div>
+          <div className="shoppinglist-text">
+            <div className="shop-title">{data[idx].title}</div>
+            <div className="shop-right">
+              <div className="shop-percent">
+                {data[idx].discountPercentage === null
+                  ? null
+                  : data[idx].discountPercentage + "%"}
+              </div>
+              <div className="shop-price">
+                {data[idx].price === null
+                  ? null
+                  : Number(data[idx].price)
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "원"}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </Col>
+      </Col>
+      {clickimg === true ? (
+        <Modal data={data} idx={idx} setClickimg={setClickimg} />
+      ) : null}
+    </>
   );
 };
+
+function Modal({ data, idx, setClickimg }) {
+  return (
+    <div className="modal-bg">
+      <div className="test">
+        <img
+          className="modal-img"
+          src={
+            data[idx].image_url === null
+              ? data[idx].brand_image_url
+              : data[idx].image_url
+          }
+          alt=""
+        />
+        <img
+          className="closebutton"
+          src={closebutton}
+          alt=""
+          onClick={() => {
+            setClickimg(false);
+          }}
+        />
+        <div className="modal-title">
+          <img src={bookstar} alt="" />
+          {data[idx].title}
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Shoppinglist;
